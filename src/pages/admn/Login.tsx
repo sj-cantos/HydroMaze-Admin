06e7065d12f5e6c5b16fe9550bc8d9404e2f5b82
shaaaaanj <shanjohncantz@gmail.com>
2024-06-12 03:56:46 +0800

    added error messages

--- src/pages/admn/Login.tsx
+++ src/pages/admn/Login.tsx
@@ -11,6 +11,7 @@ const Login = () => {
     username: '',
     password: ''
   })
+  const [error, setError] = useState('');
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const{ name, value} = e.target;
@@ -24,11 +25,14 @@ const Login = () => {
 
   const handleSubmit = async (e:React.FormEvent) => {
     e.preventDefault()
+    try{ 
     const response = await axios.post('http://127.0.0.1:4001/auth/login', adminCredentials);
     const accessToken = response.data.accessToken;
     const refreshToken = response.data.refreshToken;
     console.log("access: ",accessToken, "refresh: ", refreshToken);
-    
+    } catch (err) {
+      setError("Username or password incorrect");
+    }
   }
 
 
@@ -51,12 +55,12 @@ const Login = () => {
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in to your account
+                Administrator Log-in
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
                 <div>
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Your email
+                    Your username
                   </label>
                   <input
                     type="string"
@@ -80,29 +84,8 @@ const Login = () => {
                     onChange={handleChange}
                   />
                 </div>
-                <div className="flex items-center justify-between">
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="remember"
-                        aria-describedby="remember"
-                        type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                      />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label className="text-gray-500 dark:text-gray-300">
-                        Remember me
-                      </label>
-                    </div>
-                  </div>
-                  <a
-                    href="#"
-                    className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
+                { error && <p className="text-red-500 text-sm">{error} </p>}
+                
                 <button
                   type="submit"
                   className="w-full text-blue bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
@@ -110,15 +93,7 @@ const Login = () => {
                 >
                   Sign in
                 </button>
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Don’t have an account yet?{" "}
-                  <a
-                    href="#"
-                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  >
-                    Sign up
-                  </a>
-                </p>
+                
               </form>
             </div>
           </div>
